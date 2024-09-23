f3d6c9ae9a6b4d01fbec98ebf174be5daaef6812
ShabaMV <shabalinmv@gmail.com>
2024-09-23 12:15:27 +0300

    fix err with age

--- lesson-1-3.py
+++ lesson-1-3.py
@@ -2,7 +2,7 @@ name = "Mike"
 print(f"Name: {name}")
 age = 48
 print(f"Age: {age}")
-new_age = age + 2
-print(f"New age: {new_age}")
+age = age + 2
+print(f"New age: {age}")
 is_student = True
 print(f"Is student: {is_student}")
