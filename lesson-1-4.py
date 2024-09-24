4c96d4e373737bda563b0638c49e932e9b36249f
ShabaMV <shabalinmv@gmail.com>
2024-09-24 09:19:43 +0300

    1st try

---
+++ lesson-1-4.py
@@ -0,0 +1,10 @@
+#quest 2
+my_string = input("Введи произвольное значение строки: ")
+print("Длина строки:",len(my_string),"символов")
+
+#quest 3
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',""))
+print(my_string[0])
+print(my_string[-1])
