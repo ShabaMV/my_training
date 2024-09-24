96d4bb5db837ab295baa0a348d1c912f0e085390
ShabaMV <shabalinmv@gmail.com>
2024-09-24 09:24:40 +0300

    2nd try

--- lesson-1-4.py
+++ lesson-1-4.py
@@ -5,6 +5,8 @@ print("Длина строки:",len(my_string),"символов")
 #quest 3
 print(my_string.upper())
 print(my_string.lower())
-print(my_string.replace(' ',""))
+my_string = my_string.replace(' ',"")
+# Изменил переменную. Выводить не стал. Но если что - следующая строка
+# print(my_string)
 print(my_string[0])
 print(my_string[-1])
