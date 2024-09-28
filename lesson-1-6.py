9a3f96eae189dcefcf5ffef811ba990f6b9c1032
ShabaMV <shabalinmv@gmail.com>
2024-09-28 10:53:19 +0300

    v.1 b

--- lesson-1-6.py
+++ lesson-1-6.py
@@ -5,17 +5,20 @@ print(my_dict.get("third","Элемент не найден"))
 my_dict.update({"third":3,"forth":4})
 
 del_elem = my_dict.pop("first");
-print(my_dict,f"\r\nБыл удален элемент словаря со значением {del_elem}")
+print(f"Был удален элемент словаря со значением {del_elem}")
+print(my_dict)
 
 my_set = {1, 1, 1, 2, 3, 4, 522, 5, 436, 23, 1,True, "More", "One more", "One more", ("asasa",True)}
 print(my_set)
+# Добавляем элементы множества поочередно
 my_set.add(16)
 my_set.add(17)
 print(my_set)
 
+# Другой вариант добавления элементов в множество путем слияния
 my_new_set = {212,523,5}
-my_last_set = my_set.union(my_new_set)
-print(my_last_set)
+my_set = my_set.union(my_new_set)
+print(my_set)
 
-my_last_set.remove(1)
-print(my_last_set)
+my_set.remove(1)
+print(my_set)
