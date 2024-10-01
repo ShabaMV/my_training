54b68d897473b305aa47a1fa33d89ddc5296e274
ShabaMV <shabalinmv@gmail.com>
2024-10-01 21:22:41 +0300

    BREAK MISTAKE

--- stage 2/lesson-2-5.py
+++ stage 2/lesson-2-5.py
@@ -1,13 +1,10 @@
-# возьмем определители простых чисел, ограничив диапазон
-# наибольшим значением списка numbers
-simple_numbers = [2, 3, 5, 7, 11, 13]
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = list()
 not_primes = list()
 
 for i in numbers[1:15]:
     is_prime = True
-    for j in simple_numbers[0:i]:
+    for j in numbers[1:i]:
         if (i % j == 0) and (j < i):
             is_prime = False
             break
