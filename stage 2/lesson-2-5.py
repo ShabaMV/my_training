0aa479bad0039afa4d567bfd29501a2b1ea26d5b
ShabaMV <shabalinmv@gmail.com>
2024-10-01 21:35:01 +0300

    BREAK MISTAKE скобки аут)

--- stage 2/lesson-2-5.py
+++ stage 2/lesson-2-5.py
@@ -5,11 +5,11 @@ not_primes = list()
 for i in numbers[1:15]:
     is_prime = True
     for j in numbers[1:i]:
-        if (i % j == 0) and (j < i):
+        if i % j == 0 and j < i:
             is_prime = False
             break
 
-    if (is_prime == False):
+    if is_prime == False:
         not_primes.append(i)
     else:
         primes.append(i)
