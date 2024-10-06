90011581fc5f40f6208c7f8c5d2c9ab5a4434233
ShabaMV <shabalinmv@gmail.com>
2024-10-06 19:51:16 +0300

    v.1

--- stage 3/lesson-3-5.py
+++ stage 3/lesson-3-5.py
@@ -8,3 +8,16 @@ def get_multiplied_digits(number):
 
 result = get_multiplied_digits(40203)
 print(result)
+
+# Решил закрепить знания по рекурсии, расчетом факториала. В задание не входит.
+# Для понимания работы рекурсии.
+
+def factorial(number):
+    if number == 1:
+        return number
+    else:
+        return number * factorial(number - 1)
+
+number = int(input("Введите значение для расчета факториала: "))
+factor = factorial(number)
+print(f"{number}! = {factor}")
