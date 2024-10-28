ddcd04dbd2ae444b5d484fbc831b9d9fe575fc42
ShabaMV <shabalinmv@gmail.com>
2024-10-28 21:05:39 +0300

    v.1

--- stage 5/practice.py
+++ stage 5/practice.py
@@ -9,7 +9,19 @@ class Human():
 
     def __len__(self):
         return self.age
-    
+
+    def __lt__(self, other):
+        return self.age < other.age
+
+    def __gt__(self, other):
+        return self.age > other.age
+
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+
+    def __bool__(self):
+        return bool(self.age)
+
     def say_info(self):
         print(f"Привет, меня зовут {self.name}! Мне {self.age}")
 
@@ -19,5 +31,9 @@ class Human():
 
 den = Human("Денис", 22)
 max = Human("Максим", 18)
-print(len(den))
+print(max < den)
+print(max > den)
+print(max == den)
+if den:
+    den.say_info()
 max.birthday()
