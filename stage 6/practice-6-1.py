cb995987881afb22990facc14f5f9ea94b28f873
ShabaMV <shabalinmv@gmail.com>
2024-11-10 19:35:08 +0300

    v.1

--- stage 6/practice-6-1.py
+++ stage 6/practice-6-1.py
@@ -1,1 +1,17 @@
 class Human:
+    head = True
+    def say_hello(self):
+        print("Здравствуйте")
+
+class Student(Human):
+    def about(self):
+        print("Я студент")
+
+class Teacher(Human):
+    pass
+
+student = Student()
+teacher = Teacher()
+
+student.say_hello()
+teacher.say_hello()
